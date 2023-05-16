9be0620eac3d760e6e235afc9fcb84f11fabf766
Matúš <kollarm9@gmail.com>
2023-05-16 09:55:26 +0200

    changes to hide website

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,62 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Portfolio</h1>
       <h3>Matus Kollar</h3>
+      <div className="signature-container">
+        <svg
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 841.9 595.3"
+          className="container"
+          xmlSpace="preserve"
+        >
+          <style
+            type="text/css"
+            dangerouslySetInnerHTML={{
+              __html:
+                "\n\t.st0{fill:none;stroke:#ffffff;stroke-width:5;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}\n",
+            }}
+          />
+          <g>
+            <path
+            id="signature1"
+              className="st0"
+              d="M324.8,116.2C286.9,242,200.5,348,156.7,471.9"
+            />
+            <path
+            id="signature2"
+              className="st0"
+              d="M387.2,140.7c-29.1,58.2-63.4,113.9-102.3,166c-10.3,13.8-21.9,28-38.3,33.3c-3-15.7,6.8-32.9,21.9-38.3
+		c15.1-5.3,33.6,1.9,41.1,16.1c0.4-2.6,3.3-4.2,5.9-4.3c2.6-0.1,5.2,0.8,7.9,1.1c11.4,1.5,22.6-10.2,20.5-21.5
+		c-5.4-1.7-11.7,0.3-15,4.9c-3.3,4.6-3.3,11.3,0.1,15.8c5.8-5.7,11.7-11.4,17.5-17.1c11.9-11.6,23.8-23.2,35.6-34.7"
+            />
+            <path
+            id="signature3"
+              className="st0"
+              d="M452.5,97.5c-5.9,66.1-34,127.8-61.7,188.1c-15.7,34.1-31.3,68.2-47,102.3"
+            />
+            <path
+            id="signature4"
+              className="st0"
+              d="M482.5,147.3c-23,36.3-46.1,72.7-63.9,111.8s-30.3,81.2-31,124.1"
+            />
+            <path
+            id="signature5"
+              className="st0"
+              d="M428.5,331.8c13.3-6.3,19.4-21.3,27.7-33.3c13.6-19.7,34.7-32.7,55.8-44c58.7-31.5,122.1-54.4,187.4-67.6"
+            />
+          </g>
+        </svg>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
